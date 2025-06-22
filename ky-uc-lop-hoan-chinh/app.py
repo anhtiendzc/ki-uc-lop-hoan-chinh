1e6ff4b9d02caba0c5012292f025cd8c6c65b825
anhtiendzc <tinv65985@gmail.com>
2025-06-22 13:03:25 +0700

    app.py

--- ky-uc-lop-hoan-chinh/app.py
+++ ky-uc-lop-hoan-chinh/app.py
@@ -22,7 +22,7 @@ def index():
     return render_template('index.html', files=files)
 
 if __name__ == '__main__':
-import os
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
