4582606aaa37d42c02fcea37d8ccd481805a7344
anhtiendzc <tinv65985@gmail.com>
2025-06-22 13:24:15 +0700

    app.py

--- ky-uc-lop-hoan-chinh/app.py
+++ ky-uc-lop-hoan-chinh/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 100MB
+
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -17,12 +20,17 @@ def index():
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
-            return redirect(url_for('index'))
+            return redirect(url_for('index', filename=filename))
+
+    # Lấy danh sách file để hiển thị
     files = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('index.html', files=files)
+
+    # Lấy filename vừa upload nếu có
+    uploaded_file = request.args.get('filename')
+
+    return render_template('index.html', files=files, uploaded_file=uploaded_file)
 
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
